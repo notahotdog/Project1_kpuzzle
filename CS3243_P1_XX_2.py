a1f2a693c1258b7aa75af98729710cf098055c79
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2020-05-21 07:38:43 +0800

    minor refactor

--- CS3243_P1_XX_2.py
+++ CS3243_P1_XX_2.py
@@ -5,6 +5,8 @@ import os
 import sys
 import math
 from copy import deepcopy
+import collections
+import copy
 
 # Running script on your own - given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
@@ -15,13 +17,6 @@ class Puzzle(object):
         self.init_state = init_state
         self.goal_state = goal_state
         self.actions = list()
-
-        self.total_length = len(init_state) #length of list
-        self.nSize = int(abs(math.sqrt(len(init_state)))) #n definition of matrix
-        self.g = 0
-        
-
-
 
     def solve(self):
         #TODO
@@ -37,34 +32,83 @@ class Puzzle(object):
         #return ["LEFT", "RIGHT"] # sample output 
 
 
-
-
     # you may add more functions if you think is useful
 
-    
+    def solvable(self):
+        n = len(self.init_state)
+        if ((n % 2 == 1) and (self.count_inv(n) % 2 == 0)):
+            return True
+        elif (n % 2 == 0):
+            row = self.find_zero(self.init_state, n)
+            if (self.count_inv(n) % 2 == 0):
+                if (row % 2 == 1):
+                    return True
+            else:
+                if (row % 2 == 0):
+                    return True       
+        return False
+
+
+    def count_inv(self, n):
+        cpy = []
+        count = 0
+        for i in range(n):
+            cpy.extend(self.init_state[i])
+        for i in range(n*n):
+            for j in range(i + 1, n*n):
+                if ((cpy[i] > cpy[j]) and (cpy[j] != 0)):
+                    count += 1
+        return count
+
+    def find_zero(self, state, n):
+        for i in range(n):
+            for j in range(n):
+                if (state[i][j] == 0):
+                    return n - i
+
+
     def a_star_search(self):
 
         #pass the goal state and the initial state
 
-        queue = collections.deque(init_state) #add the initial state
+        queue = collections.deque(Node(init_state,goal_state,0)) #add the initial state
 
         while queue:
-            if(solved):
+
+            tempNode = queue.popLeft()
+
+            if(solved(self.init_state)):
                 return self.actions #can be changed later on
             
             else:
+
+                #create a new node to be added into the queue to be decremented from
+                #check all the possible actions for this node
+                actionCheck = tempNode.validActions
+                Node(tempNode)
+
+
+
                 return False
                 #incorporate action that adds to path
-         
 
-    
     #checks whether puzzle has been solved - need to ensure values inside are equal
     def solved(self, tempState):
-        return tempState == self.goal_state
+        return tempState == self.goal_state     
 
+class  Node(object):
+    def __init__(self, initial_state,goal_state,g): #initial state and the g value
+        # you may add more attributes if you think is useful
+        self.total_length = len(initial_state) #length of list
+        self.nSize = int(abs(math.sqrt(len(initial_state)))) #n definition of matrix
+        self.g = 0; 
+        
     #number of steps taken to get to current state
     def g(self):
         return g 
+
+    def inc_g(self):
+        self.g += 1
 
     #manhattan distance to get to next state
     def h(self):
@@ -78,7 +122,7 @@ class Puzzle(object):
 
         temp_copy = copy.deepcopy(init_state) #makes a copy of the initial state
 
-        coordinates = findZero(temp_copy)
+        coordinates = findZeroCoordinates(temp_copy)
         xVal = coordinates[0]
         yVal = coordinates[1]
 
@@ -94,7 +138,7 @@ class Puzzle(object):
 
     
     #returns a pair that indicates the x and y of 0
-    def findZero(state):
+    def findZeroCoordinates(state):
         xCtr = 0
         yCtr = 0
 
@@ -123,37 +167,9 @@ class Puzzle(object):
 
 
 
-    def count_inv(self, n):
-        cpy = []
-        count = 0
-        for i in range(n):
-            cpy.extend(self.init_state[i])
-        for i in range(n*n):
-            for j in range(i + 1, n*n):
-                if ((cpy[i] > cpy[j]) and (cpy[j] != 0)):
-                    count += 1
-        return count
+    
 
-    def find_zero(self, state, n):
-        for i in range(n):
-            for j in range(n):
-                if (state[i][j] == 0):
-                    return n - i
-
-    def solvable(self):
-        n = len(self.init_state)
-        
-        if ((n % 2 == 1) and (self.count_inv(n) % 2 == 0)):
-            return True
-        elif (n % 2 == 0):
-            row = self.find_zero(self.init_state, n)
-            if (self.count_inv(n) % 2 == 0):
-                if (row % 2 == 1):
-                    return True
-            else:
-                if (row % 2 == 0):
-                    return True       
-        return False
+    
             
 if __name__ == "__main__":
     # do NOT modify below
